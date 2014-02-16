9a0984d405d75f70032e375b364c5a3af4ae621a
Ray McDermott <ray.mcdermott@toyota-europe.com>
2014-02-16 15:16:34 +0100

    Added support to read app list per org from a file (Hack!)
    Moved cmd line arguments to env vars
    
    Fixed up docs accordingly

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ A node command line utility to copy Heroku slugs from one app to one or more oth
 
 This is valuable when you want the same code in different Heroku apps to partition your system (for example by country)
 
-FOr details of the Heroku platform API see
+For details of the Heroku platform API see
 
 https://blog.heroku.com/archives/2013/12/20/programmatically_release_code_to_heroku_using_the_platform_api
 
@@ -14,15 +14,33 @@ Install the command line utility with: `npm install -g copy-heroku-releases`
 
 You must have an account on Heroku
 
-You must provide the Heroku API key for your account in the environment variable HEROKU_API_TOKEN
-
-You must provide the list of target apps in the environment variable TARGET_APPS
-
 ## Documentation
 
-This program is given the name of an application that you own on Heroku and [optionally] a release number
+This program will deploy a slug from one application to one or more others. You must own these apps.
 
 The default is to copy the most recent (highest) release
+
+The program accepts its parameters from the environment rather than the command line.
+
+## Environment variables
+
+Mandatory parameters
+
+The source application name: DEPLOY_SOURCE_APPLICATION
+
+The Heroku API key for your account: DEPLOY_HEROKU_API_TOKEN
+
+The target organisation: DEPLOY_TARGET_ORGANISATION
+
+The list of target apps: DEPLOY_TARGET_APP_FILTER
+
+Note: the target apps can be a list of regular app names or a list of regexes or a mix of both
+
+Optional parameters
+
+You can run a rehearsal of the deployment by setting DEPLOY_REHEARSAL (any value). This will list the action that the program would take but prevents execution.
+
+You can request an explicit release setting DEPLOY_RELEASE_NUMBER. This will use the specific release rather than the latest.
 
 ## Errors
 
@@ -32,17 +50,20 @@ If the requested release number is not present this is reported as an error
 
 ## Examples
 
-$ copy-heroku-releases test-deploy-app
+$ copy-heroku-releases
 
-Copied slug 6b938191-f5a3-4033-a916-xyz to app test-deploy-xyz [created new app version 10]
+Deploying from test-deploy-app release 27 ('stable feature xxx v0.3.1') with slug 4bd9fcde-c6b0-499d-9029
 
-Copied slug 6b938191-f5a3-4033-a916-xyz to app test-deploy-def [created new app version 9]
+Copied slug 4bd9fcde-c6b0-499d-9029 to app test-deploy-xyz [created new app version 36]
+
+Copied slug 4bd9fcde-c6b0-499d-9029 to app test-deploy-xyz [created new app version 35]
 
 ## Contributing
 
 In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com/).
 
 ## Release History
+0.8.3 - hack to support deploying apps by organisation
 0.8.2 - relies on the updated Heroku client now ... and I have more TODOs!
 0.8.1 - realised that it wasn't fully functional ... proxy patch
 0.8.0 - fully functional for me ... not 1.0.0 as I haven't socialised this enough yet :)
--- copy-heroku-releases.js
+++ copy-heroku-releases.js
@@ -2,6 +2,8 @@
 
 'use strict';
 
+// TODO: Write unit tests
+
 /*
  * copy-heroku-releases.js
  * https://github.com/raymcdermott/copy-heroku-releases.js
@@ -11,42 +13,65 @@
  */
 
 var assert = require('assert');
+var lazy = require('lazy.js');
+var herokuClient = require('heroku-client');
 
-// Command line options
-assert((process.argv.length == 3 || process.argv.length == 4), "You must specify appname [optional-release-number]");
-
-var app = process.argv[2];
-var requestedRelease = parseInt(process.argv[3]);
 
 // Mandatory environment variables
-assert(process.env.HEROKU_API_TOKEN, "You must have HEROKU_API_TOKEN set in your environment");
-assert(process.env.TARGET_APPS, "You must have TARGET_APPS set in your environment");
+assert(process.env.DEPLOY_HEROKU_API_TOKEN, "You must set DEPLOY_HEROKU_API_TOKEN in your environment");
+assert(process.env.DEPLOY_SOURCE_APPLICATION, "You must set DEPLOY_SOURCE_APPLICATION in your environment");
+assert(process.env.DEPLOY_TARGET_ORGANISATION, "You must set DEPLOY_TARGET_ORGANISATION in your environment");
+assert(process.env.DEPLOY_TARGET_APP_FILTER, "You must set DEPLOY_TARGET_APP_FILTER in your environment");
+
+var sourceApp = process.env.DEPLOY_SOURCE_APPLICATION;
+var targetOrganisation = process.env.DEPLOY_TARGET_ORGANISATION;
+var targetAppFilter = process.env.DEPLOY_TARGET_APP_FILTER;
 
 
-var Heroku = require('heroku-client'),
-    heroku = new Heroku({ token: process.env.HEROKU_API_TOKEN });
+// Optional environment variables
+var rehearsal = process.env.DEPLOY_REHEARSAL;
+var requestedRelease = process.env.DEPLOY_RELEASE_NUMBER;
+
+// Heroku set up requires authentication
+var heroku = new herokuClient({ token: process.env.DEPLOY_HEROKU_API_TOKEN });
 
 
-processSlug(app, requestedRelease);
+// Obtain the releases from Heroku to kick us off...
+
+heroku.get('/apps/' + sourceApp + '/releases/', function (err, responseBody) {
+    assert.ifError(err, "Could not get the releases from Heroku");
+
+    deployRelease(sourceApp, responseBody, requestedRelease);
+});
 
 
-// fetch the slug ids and execute the copying
-function processSlug(appName, requestedRelease) {
-    heroku.get('/apps/' + appName + '/releases/', function (err, responseBody) {
-        assert.ifError(err, "Could not get the releases from Heroku");
+function deployRelease(sourceApp, releases, requestedRelease) {
 
-        var release = getRelease(responseBody, requestedRelease);
+    var release = findReleaseToDeploy(releases, requestedRelease);
 
-        var slug = (getSlug(release));
+    var slug = findSlug(release);
 
-        copySlug(slug);
-    });
+    console.log("Deploying from " + sourceApp + " release " + release.version + " (\'" + release.description + "\') with slug " + slug);
+
+    var targetApps = getAppsForOrganisation(targetOrganisation);
+
+    if (targetAppFilter) {
+        targetAppFilter = targetAppFilter.split(" ");
+        targetApps = filterTargetApps(targetApps, targetAppFilter);
+    }
+
+    if (rehearsal) {
+        console.log("Rehearsal mode ... would deploy slug to the following apps in organisation " + targetOrganisation);
+        for (var targetApp = 0; targetApp < targetApps.length; targetApp++) {
+            console.log(targetApps[targetApp]);
+        }
+        return 0;
+    }
+
+    deploySlug(slug, targetApps);
 }
 
-
-function getRelease(releases, requestedRelease) {
-    var lazy = require('lazy.js');
-
+function findReleaseToDeploy(releases, requestedRelease) {
     if (requestedRelease) {
         return lazy(releases).find(function (release) {
             return release.version === requestedRelease;
@@ -60,7 +85,7 @@ function getRelease(releases, requestedRelease) {
 }
 
 
-function getSlug(release) {
+function findSlug(release) {
     if (!release || !release.slug) {
         throw new Error("Cannot find a valid slug ID in release " + JSON.stringify(release));
     }
@@ -69,9 +94,7 @@ function getSlug(release) {
 }
 
 
-function copySlug(slug) {
-    var targetApps = process.env.TARGET_APPS.split(" ");
-
+function deploySlug(slug, targetApps) {
     for (var i = 0; i < targetApps.length; i++) {
         var appName = targetApps[i];
 
@@ -83,10 +106,37 @@ function copySlug(slug) {
     }
 }
 
-// TODO: Support regex for target apps
-// TODO: Support deploying against another organisation
-function getAppsForOrganisation() {
-// Not available yet
+// Filter the list of applications using the array of regular expressions
+function filterTargetApps(appList, regexFilter) {
+    if (!regexFilter)
+        return appList;
+
+    return lazy(appList).filter(function (name) {
+
+        for (var i = 0; i < regexFilter.length; i++) {
+            if (name.match(regexFilter[i])) return true;
+        }
+
+        return false;
+
+    }).toArray(); // TODO: why is this needed ... returns list + lazy.js function names if not used :(
+}
+
+// BIG MESS ... until Heroku supports getting the apps by organisation in the API, we will hack it...
+//
+// $ heroku apps --org ${org} | egrep -v "^$|${org}$" | awk '{ print $1 }' > /tmp/heroku-app-list.${org}
+//
+// will give us a list of app names for the $org into the file
+//
+// I am making this a deliberately ugly hard coded hack!!
+//
+// TODO: Refactor once this is in the API
+function getAppsForOrganisation(organisation) {
+    var fs = require('fs');
+
+    var appListFile = "/tmp/heroku-app-list." + organisation;
+
+    return fs.readFileSync(appListFile).toString().split("\n");
 }
 
 
